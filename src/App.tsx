0458c10bb1eb6a241bf199974604454d2761c0a3
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-02-11 10:24:30 +0100

    remove StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header"
+import Header from "./components/Header"
 import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Page1 from "./views/Page1";
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,7 +5,5 @@ import "./App.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
